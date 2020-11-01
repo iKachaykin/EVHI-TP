597f1c79257df42cb9e863aa079c2543ab907357
iKachaykin <ivankachaykin707@gmail.com>
2020-11-02 00:37:27 +0200

    Second level enemy update and stage 3
    The enemy of the second level now causes damage and push player out on collision. Moreover, first version of stage 3 is also provided.

---
+++ Assets/Scripts/CauseDamageOnCollision.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CauseDamageOnCollision : MonoBehaviour
+{
+    public int damage = 10;
+
+    private float reloadProgress = 0f, reloadLimit = 2f;
+    private Health receiverHealth;
+    private float eps = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        reloadProgress = reloadLimit + eps;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        reloadProgress += Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        OnCollision(collision);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        OnCollision(collision);
+    }
+
+    void OnCollision(Collision collision)
+    {
+        if (reloadProgress > reloadLimit)
+        {
+            reloadProgress = 0f;
+            receiverHealth = collision.gameObject.GetComponent<Health>();
+            if (receiverHealth != null)
+                receiverHealth.GetDamage(damage);
+        }
+    }
+}
---
+++ Assets/Scripts/PushPlayerOut.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushPlayerOut : MonoBehaviour
+{
+    public float absForce = 2000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody colrb = collision.gameObject.GetComponent<Rigidbody>();
+        if (colrb != null)
+            colrb.AddForce(absForce * (new Vector3(-collision.gameObject.transform.forward.x, 0f, -collision.gameObject.transform.forward.z)));
+    }
+}
